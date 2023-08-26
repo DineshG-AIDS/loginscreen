f1b62301bf4c5bad0ba699019f2cc58de1312215
DINESHfing <99419646+DINESHfing@users.noreply.github.com>
2023-08-26 11:47:00 +0530

    TEst compledted

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const registerButton = document.getElementById('register')
-const loginButton = document.getElementById('login')
-const container = document.getElementById('container')
- 
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+const email = document.getElementById("email1");
 
-registerButton.onclick = function(){
-	 container.className = 'active'
-}
-loginButton.onclick = function(){
-		container.className = 'close'
-}
+registerButton.onclick = function() {
+  container.className = "active";
+};
+loginButton.onclick = function() {
+  container.className = "close";
+};
